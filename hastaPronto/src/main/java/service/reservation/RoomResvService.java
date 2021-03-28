55ab407de3fb6b5226f25a93a1b9af3eb7bafe7c
jhy118 <jhyong118@gmail.com>
2021-03-29 07:57:51 +0900

    add reservation

---
+++ hastaPronto/src/main/java/service/reservation/RoomResvService.java
@@ -0,0 +1,16 @@
+package service.reservation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+public class RoomResvService {
+
+	public void execute(String rmNo, String chIn) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
