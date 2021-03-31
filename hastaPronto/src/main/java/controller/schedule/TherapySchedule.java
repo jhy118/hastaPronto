aa695f652c7bff50c0236ddf885fd275bd02b6d8
Elean2021 <ezen@DESKTOP-O03MT84>
2021-03-31 20:30:25 +0900

    Elean2021(Therapy)

--- hastaPronto/src/main/java/controller/schedule/TherapySchedule.java
+++ hastaPronto/src/main/java/controller/schedule/TherapySchedule.java
@@ -1,28 +1,46 @@
 package controller.schedule;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import command.ScheduleCommand;
+import command.WedCsCommand;
+import service.schedule.TherapyCalendarListService;
 import service.schedule.TherapyListService;
+import validator.ScheduleCommandValidator;
 
 @Controller
-@RequestMapping("schedule")
+@RequestMapping("schedule/therapy")
 public class TherapySchedule {
-//	@Autowired
-//	TherapyListService therapyListService;
-	
-	@RequestMapping(value = "therapyList", method = RequestMethod.GET)
-	public String therapy() {
-//		therapyListService.execute(page, model);
+	@Autowired
+	TherapyListService therapyListService;
+	@Autowired
+	TherapyCalendarListService therapyCalendarListService;
+
+	@RequestMapping("therapyList")
+	public String therapyList(Model model, ScheduleCommand scheduleCommand) {
+		TherapyCalendarListService.execute(model, scheduleCommand);
 		return "schedule/therapy/therapyList";
 	}
-//	@RequestParam(value="page", defaultValue = "1") Integer page, Model model
 	@RequestMapping("therapyRvForm")
-	public String therapyRvForm() {
+	public String therapyRvForm(@ModelAttribute("ScheduleCommand") ScheduleCommand scheduleCommand) {
 		return "schedule/therapy/therapyRvForm";
 	}
+	@RequestMapping(value="therapyRvAction", method=RequestMethod.POST)
+	public String therapyRvAction(ScheduleCommand scheduleCommand, Errors errors, HttpServletRequest request) {
+		new ScheduleCommandValidator().validate(scheduleCommand, errors);
+	if(errors.hasErrors()) {
+		return "schedule/therapy/therapyRvForm";
+		}
+	therapyListService.execute(scheduleCommand, request);
+	return "redirect:/sschedule/therapy/therapyList";
+	}
+	
 }
--- hastaPronto/src/main/webapp/WEB-INF/view/schedule/main.jsp
+++ hastaPronto/src/main/webapp/WEB-INF/view/schedule/main.jsp
@@ -7,7 +7,7 @@
 <title>schedule/main.jsp</title>
 </head>
 <body>
-<a href="therapyList">Therapy 자세히보기</a><br />
+<a href="therapy/therapyList">Therapy 자세히보기</a><br />
 <a href="fitnessList">Fitness 자세히보기</a><br />
 <a href="swimmingList">Swimming 자세히보기</a><br />
 </body>
