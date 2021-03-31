dd8e8d4ff85c87c595b87c2e298ee1d30fc4e427
ezen <ezen@DESKTOP-O03MT84>
2021-03-31 17:35:38 +0900

    wedding Reservation complete

--- hastaPronto/src/main/java/controller/wedRv/WedRvController.java
+++ hastaPronto/src/main/java/controller/wedRv/WedRvController.java
@@ -1,13 +1,85 @@
 package controller.wedRv;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import command.WedRvCommand;
+import service.wedRv.WedRvDeleteService;
+import service.wedRv.WedRvFormService;
+import service.wedRv.WedRvInfoService;
+import service.wedRv.WedRvListService;
+import service.wedRv.WedRvModifyService;
+import service.wedRv.WedRvOkService;
+import validator.WedRvCommandValidator;
 
 @Controller
 @RequestMapping("wedRv")
 public class WedRvController {
+	@Autowired
+	WedRvOkService wedRvOkService;
+	@Autowired
+	WedRvFormService wedRvFormService;
+	@Autowired
+	WedRvListService wedRvListService;
+	@Autowired
+	WedRvInfoService wedRvInfoService;
+	@Autowired
+	WedRvModifyService wedRvModifyService;
+	@Autowired
+	WedRvDeleteService wedRvDeleteService;
+	@RequestMapping("wedRvList")
+	public String wedRvList(WedRvCommand wedRvCommand, Model model) {
+		wedRvListService.execute(wedRvCommand, model);
+		return "wedRv/wedRvList";
+	}
 	@RequestMapping("wedRvForm")
-	public String wedRvForm() {
+	public String wedRvForm(WedRvCommand wedRvCommand, Model model) {
+		wedRvFormService.execute(model);
 		return "wedRv/wedRvForm";
 	}
+	@RequestMapping("wedRvOk")
+	public String wedRvOk(@ModelAttribute(value="wedRvCommand")WedRvCommand wedRvCommand, 
+			Errors errors, HttpSession session) {
+		new WedRvCommandValidator().validate(wedRvCommand, errors);
+		if(errors.hasErrors()) {
+			return "wedRv/wedRvForm";
+		}
+		wedRvOkService.execute(wedRvCommand, session);
+		return "wedRv/wedRvOk";
+	}
+	@RequestMapping("wedRvInfo")
+	public String wedRvInfo(
+			@RequestParam(value="wedRvNo") String wedRvNo,
+			Model model, WedRvCommand wedRvCommand) {
+		wedRvInfoService.execute(wedRvNo, model);
+		wedRvListService.execute(wedRvCommand, model);
+		return "wedRv/wedRvInfo";
+	}
+	@RequestMapping("wedRvModify")
+	public String wedRvModify(
+			@RequestParam("wedRvNo") String wedRvNo,
+			Model model, WedRvCommand wedRvCommand) {
+		wedRvInfoService.execute(wedRvNo, model);
+		return "wedRv/wedRvModify";
+	}
+	@RequestMapping("wedRvModifyOk")
+	public String wedRvModifyOk(WedRvCommand wedRvCommand) {
+		wedRvModifyService.execute(wedRvCommand);
+		return "redirect:wedRvInfo?wedRvNo="
+				+wedRvCommand.getWedRvNo();
+	}
+	@RequestMapping("wedRvDel")
+	public String wedRvDel(
+			@RequestParam(value="wedRvNo") String wedRvNo) {
+		wedRvDeleteService.execute(wedRvNo);
+		return "redirect:wedRvList";
+	}
+	
 }
--- hastaPronto/src/main/webapp/WEB-INF/view/wedRv/wedRvList.jsp
+++ hastaPronto/src/main/webapp/WEB-INF/view/wedRv/wedRvList.jsp
@@ -5,16 +5,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>wedCsList</title>
+<title>wedRvList.jsp</title>
 </head>
 <body>
 <table border="1">
-<caption>웨딩 상담 일정 예약하기</caption>
-	<tr>
+<caption>웨딩 & 레스토랑 예약하기</caption>
+<tr>
 		<td colspan="7" align="center">
-			<a href="wedCsList?nYear=${nYear}&nMonth=${nMonth}&action=previous">이전달</a> 
+			<a href="wedRvList?nYear=${nYear}&nMonth=${nMonth}&action=previous">이전달</a> 
 			${nYear}년 ${nMonth}월
-			<a href="wedCsList?nYear=${nYear}&nMonth=${nMonth}&action=next">다음달</a>
+			<a href="wedRvList?nYear=${nYear}&nMonth=${nMonth}&action=next">다음달</a>
 		</td>
 	</tr>
 	<tr>
@@ -25,7 +25,7 @@
 				<c:set var="value2" value="${num-maxDay-1}" />
 				<c:if test="${num lt dayNum}"></c:if>
 				<c:if test="${num ge dayNum && num lt dayNum + maxDay}">
-					<a href="wedCsForm?nYear=${nYear}&nMonth=${nMonth}&nDay=${value1}">
+					<a href="wedRvForm?nYear=${nYear}&nMonth=${nMonth}&nDay=${value1}">
 						${value1}
 					</a>
 				</c:if>
@@ -36,12 +36,12 @@
 						&& dto.nMonth eq nMonth 
 						&& dto.nDay eq fn:replace(value1, ' ','')}">
 						<c:if test="${authInfo.grade == 'emp' }">
-							<a href='wedCsInfo?wedCosNo=${dto.wedCosNo}';">
-								${dto.wedCosNo}/${dto.wedCosName} 예약완료
+							<a href='wedRvInfo?wedRvNo=${dto.wedRvNo}&nYear=${nYear}&nMonth=${nMonth}&nDay=${value1}';">
+								${dto.wedRvNo} / ${dto.wedRvTime} 예약완료
 							</a>
 						</c:if>
 						<c:if test="${authInfo.grade != 'emp' }">
-								${dto.wedCosNo}/${dto.wedCosName} 예약완료<br />
+								${dto.wedRvNo} / ${dto.wedRvTime} 예약완료
 						</c:if>
 					</c:if>
 				</c:forEach>
@@ -50,6 +50,7 @@
 		<c:if test="${num % 7 == 0 && num != 42}"></tr><tr></c:if>
 	</c:forEach>
 	</tr>
+
 </table>
 </body>
 </html>
